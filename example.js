204b6931809aa465da2a725b6d32678edc9c7cfb
ahob85 <aaron@ahob85.com>
2018-05-26 13:33:17 -0700

    update

--- example.js
+++ example.js
@@ -3,8 +3,9 @@ var p5Canvas;
 var myName;
 var ballX;
 var ballY;
-var ballDiameter;
+var ballRadius;
 var ballXVelocity;
+var ballGrowth;
 
 // initialize global variables in setup() function
 function setup() {
@@ -14,8 +15,9 @@ function setup() {
   myName.html("Teacher");
   ballX = width / 2;
   ballY = height / 2;
-  ballDiameter = 100;
+  ballRadius = 50;
   ballXVelocity = 10;
+  ballGrowth = 10;
 }
 
 
@@ -25,10 +27,20 @@ function draw() {
 }
 
 function drawBall() {
-  var ballRadius = ballDiameter / 2; // local variable, doesn't exist outside this function
   if(ballX >= width - ballRadius || ballX <= 0 + ballRadius) {
     ballXVelocity *= -1;
   }
+  var circumference = getCircumference(); // local variable
+  if(circumference >= 600 || circumference <= 160) {
+    ballGrowth *= -1;
+  }
   ballX += ballXVelocity;
+  ballRadius += ballGrowth;
+  var ballDiameter = ballRadius * 2; // local variable
+  fill("orange");
   ellipse(ballX, ballY, ballDiameter, ballDiameter);
+}
+
+function getCircumference() {
+  return 2 * PI * ballRadius;
 }
